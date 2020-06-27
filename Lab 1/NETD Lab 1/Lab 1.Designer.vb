96982888cd51012d8207410787988f2e7b70307a
Angus Wai <anguswai1999@gmail.com>
2020-06-27 16:25:11 -0400

    Added Lab 1

---
+++ Lab 1/NETD Lab 1/Lab 1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.ttToopTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(157, 305)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(109, 33)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.ttToopTip.SetToolTip(Me.btnReset, "Reset everything")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.Location = New System.Drawing.Point(35, 305)
+        Me.btnEnter.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(109, 33)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.ttToopTip.SetToolTip(Me.btnEnter, "Input value")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(283, 305)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(109, 33)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.ttToopTip.SetToolTip(Me.btnExit, "Quit")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(172, 18)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(87, 28)
+        Me.txtInput.TabIndex = 1
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUnits.Location = New System.Drawing.Point(93, 21)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(56, 24)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "&Units:"
+        Me.lblUnits.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttToopTip.SetToolTip(Me.lblUnits, "Units Shipped")
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDay.Location = New System.Drawing.Point(279, 18)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(57, 24)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day 1"
+        Me.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttToopTip.SetToolTip(Me.lblDay, "Which day the value is going to be stored into")
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.BackColor = System.Drawing.SystemColors.Window
+        Me.txtDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDisplay.Location = New System.Drawing.Point(99, 62)
+        Me.txtDisplay.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtDisplay.Multiline = True
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.ReadOnly = True
+        Me.txtDisplay.Size = New System.Drawing.Size(239, 191)
+        Me.txtDisplay.TabIndex = 3
+        Me.txtDisplay.TabStop = False
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(99, 255)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(239, 28)
+        Me.lblOutput.TabIndex = 4
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(432, 351)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.btnReset)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(450, 398)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(399, 398)
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ttToopTip.SetToolTip(Me, "Nice")
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents ttToopTip As ToolTip
+End Class
